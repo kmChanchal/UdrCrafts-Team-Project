c530019fcadf666e8d1f3ea0006232e5cdbb4ab5
Chanchal <cbundela57@gmail.com>
2025-10-06 21:56:28 +0530

    https://github.com/Chanchalbundela/UdrCrafts-Team-Project.git

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home/index.jsx'
+
 const App = () => {
   return (
     <>
-    <BrowserRouter>
-     <Header /> 
-     <Routes>
-      <Route path={'/'} exact={true} element={<Home/>} />
-     </Routes>
-     </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path={'/'} exact={true} element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
